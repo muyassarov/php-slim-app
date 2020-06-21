a55f1e9f7577661225faf5ec9ea8f75058448aa9
Behruz Muyassarov <muyassarov@gmail.com>
2020-06-21 21:22:01 +0500

    Added users list + search

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -18,18 +19,30 @@ $app->addErrorMiddleware(true, true, true);
 $app->get('/', function (Request $request, Response $response) {
     return $response->getBody()->write('Welcome to Slim!');
 });
-$app->get('/users', function (Request $req, Response $res) {
-    return $res->write('GET /users');
+
+$app->get('/users', function (Request $req, Response $res) use ($users) {
+    $term = $req->getQueryParam('term');
+    $params = [];
+    if ($term) {
+        $filteredUsers = array_filter($users, function ($user) use ($term) {
+            return strpos($user, $term) !== false;
+        });
+        $params['users'] = $filteredUsers;
+    } else {
+        $params['users'] = $users;
+    }
+    $params['term'] = $term;
+    return $this->get('renderer')->render($res, 'users/index.phtml', $params);
 });
-$app->post('/users', function (Request $req, Response $res) {
-    return $res->withStatus(302);
-});
+
 $app->get('/users/{id}', function (Request $req, Response $res, $args) {
     $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
     return $this->get('renderer')->render($res, 'users/show.phtml', $params);
 });
+
 $app->get('/courses/{id}', function (Request $req, Response $res, $args) {
     ['id' => $id] = $args;
     return $res->write("Course id: {$id}");
 });
+
 $app->run();
