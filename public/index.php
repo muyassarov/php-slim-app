f68e840801bffb0f82c36ad9b176f5fed63e4c50
Behruz Muyassarov <muyassarov@gmail.com>
2020-06-21 17:49:57 +0500

    Added users routes

--- public/index.php
+++ public/index.php
@@ -5,12 +5,31 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
+    return $response->getBody()->write('Welcome to Slim!');
+});
+$app->get('/users', function (Request $req, Response $res) {
+    return $res->write('GET /users');
+});
+$app->post('/users', function (Request $req, Response $res) {
+    return $res->withStatus(302);
+});
+$app->get('/users/{id}', function (Request $req, Response $res, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($res, 'users/show.phtml', $params);
+});
+$app->get('/courses/{id}', function (Request $req, Response $res, $args) {
+    ['id' => $id] = $args;
+    return $res->write("Course id: {$id}");
 });
 $app->run();
